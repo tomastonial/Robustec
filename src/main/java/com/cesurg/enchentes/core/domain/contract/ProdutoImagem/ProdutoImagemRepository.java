17beee7aefcfc29315dfc042a1e2709c9509da70
cassiano richato <cassianorichato@gmail.com>
2025-11-09 19:06:29 -0300

    agora pode adicionar mais de uma imagem por produto

--- src/main/java/com/cesurg/enchentes/core/domain/contract/ProdutoImagem/ProdutoImagemRepository.java
+++ src/main/java/com/cesurg/enchentes/core/domain/contract/ProdutoImagem/ProdutoImagemRepository.java
@@ -1,4 +1,13 @@
 package com.cesurg.enchentes.core.domain.contract.ProdutoImagem;
 
+import com.cesurg.enchentes.core.domain.entity.ProdutoImagem;
+
+import java.util.List;
+
 public interface ProdutoImagemRepository {
+    public ProdutoImagem create(ProdutoImagem produtoImagem);
+    public List<ProdutoImagem> read();
+    public ProdutoImagem readById(int id);
+    public ProdutoImagem update(int id, ProdutoImagem produtoImagem);
+    public void delete(int id);
 }
